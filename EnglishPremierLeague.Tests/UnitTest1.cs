962715747bc6b3e990565b7f8bbd722b9a85ed1d
Ela <elamathi@gmail.com>
2018-10-29 03:02:47 +1100

    intial commit

---
+++ EnglishPremierLeague.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using EnglishPremierLeague;
+
+namespace EnglishPremierLeague.Tests
+{
+    public class UnitTest1 : IClassFixture<Program>
+    {
+        //[Fact(Skip ="Testing")]        
+        
+        //public void Test1()
+        //{
+        //    Program p = new Program();
+        //    Assert.Equal(4, p.Add(2, 2));
+        //}
+
+        //[Theory]
+        //[InlineData(2,2,4)]
+        //[InlineData(1, 2, 3)]
+        //[InlineData(2, -1, 1)]
+        //[InlineData(2, -12, -10)]
+        //public void Test2(int value1, int value2, int expected)
+        //{
+        //    Program p = new Program();
+        //    Assert.Equal(expected, p.Add(value1, value2));
+        //}
+    }
+}
--- EnglishPremierLeague/Program.cs
+++ EnglishPremierLeague/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 
 namespace EnglishPremierLeague
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<ITestInterface, TestClass>()
+                .AddSingleton<ILoggerFactory, LoggerFactory>()
+                .AddSingleton(typeof(ILogger<>), typeof(Logger<>))
+                .BuildServiceProvider();
+
+
+			serviceProvider.GetService<ILoggerFactory>().AddConsole(LogLevel.Debug);//.AddDebug();
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+            .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+            var bar = serviceProvider.GetService<ITestInterface>();
+            bar.TestMethod();
+
+            logger.LogDebug("All done!");
+
+            Console.ReadLine();
         }
+
+
+
+        //public int Add(int x, int y)
+        //{
+        //    return (x + y);
+        //}
+
+        //public int Subtract(int x, int y)
+        //{
+        //    return x - y;
+        //}
+    }
+
+
+    public interface ITestInterface
+    {
+        void TestMethod();
+    }
+
+    public class TestClass : ITestInterface
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("Called TestMethod");
+        }
+       
     }
 }
